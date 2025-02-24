20783dd3764157485bb92bebf0b7f983d7305aa9
Daniel De la rosa <danndelarosam@gmail.com>
2025-02-24 16:20:09 -0500

    changes to the main file

--- lib/sponsor_carousel.dart
+++ lib/sponsor_carousel.dart
@@ -1,41 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class SponsorCarousel extends StatelessWidget {
   const SponsorCarousel({super.key});
 
   final List<String> sponsorImages = const [
-    'assets/sponsors/AACBI.svg',
-    'assets/sponsors/ALCALDIA.svg',
-    'assets/sponsors/CAUJARAL.svg',
-    'assets/sponsors/HOTEL.svg',
-    'assets/sponsors/INTERFILMS.svg',
+    'assets/sponsors/CAUJARAL.png',
+    'assets/sponsors/HOTEL.png',
+    'assets/sponsors/INTERFILMS.png',
+    'assets/sponsors/sponsor1.png',
   ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 100, // Set a fixed height for the footer
+
       width:
           double
               .infinity, // Make the container take the full width of the screen
+
       padding: const EdgeInsets.symmetric(vertical: 8),
+
       color: const Color(0xFF1A2141), // Blue background
+
       child: CarouselSlider(
         options: CarouselOptions(
           autoPlay: true,
+
           enlargeCenterPage: true,
+
           aspectRatio: 2.0, // Adjust aspect ratio to fit the smaller height
         ),
+
         items:
             sponsorImages.map((image) {
-              return SvgPicture.asset(
-                colorFilter: const ColorFilter.mode(
-                  Colors.yellow,
-                  BlendMode.srcIn,
-                ),
+              return Image.asset(
                 image,
+
                 fit:
                     BoxFit
                         .contain, // Adjust fit to contain within the smaller height
